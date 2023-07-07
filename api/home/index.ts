3673e860fbd016726fe13965a052bf324772edec
David Whitney <david@davidwhitney.co.uk>
2023-07-07 17:27:10 +0100

    Simpler temp cache

--- api/home/index.ts
+++ api/home/index.ts
@@ -8,29 +8,18 @@ dotenv.config();
 
 // Hack a cheap In Memory cache in here for dev.
 
-const cache = new Array<any>();
+let assets: any = null;
 
 export async function run(context: Context, req: HttpRequest): Promise<void> {
-
-    if (cache.length > 0) {
-        context.res = {
-            status: 200,
-            headers: { "content-type": "application/json" },
-            body: JSON.stringify(cache[0])
-        };
-        return;
+    if (assets === null) {
+      const credential = new DefaultAzureCredential();
+      const source1 = new AzureDataSource(credential);      
+      assets = await source1.collectAssets();
     }
-
-    const credential = new DefaultAzureCredential();
-    const source1 = new AzureDataSource(credential);
-    
-    const assets = await source1.collectAssets();
 
     const firstKey = assets.keys().next().value;
     const firstSub = assets.get(firstKey)!;
     const grouped = groupByOwner(firstSub);
-
-    cache.push(grouped);
 
     context.res = {
       status: 200,
